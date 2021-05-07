b0c48ea157e82126a0d1ae3b6820e459adaebed9
manimekalai30 <megaladev6@gmail.com>
2021-05-07 12:25:03 +0530

    feat: Added calculate functionality

--- index.js
+++ index.js
@@ -36,6 +36,24 @@ function leapYear(year){
     return IsLeapYear;
 };
 
+function calculate(year){
+    console.log("Executed")
+    let IsLeapYear = false;
+    // Check leap year or not
+    if(year%4 == 0){
+        if(year%100 == 0){
+            if(year%400 == 0){
+                IsLeapYear = true;
+            }
+        }else{
+            IsLeapYear = true;
+        }
+    }else{
+        IsLeapYear = false;
+    }
+    return IsLeapYear;
+};
+
 module.exports.addition = addition;
 module.exports.multiplication = multiplication;
 module.exports.leapYear = leapYear;
